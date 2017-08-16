ba7a2e1c083021a7fb1ddfa8ed4d622dd508c140
海天 <476183603@qq.com>
2017-08-17 00:05:51 +0800

    进行了部分修改

--- library/src/main/kotlin/loadinglibrary/view/HTLoading.kt
+++ library/src/main/kotlin/loadinglibrary/view/HTLoading.kt
@@ -1,9 +1,12 @@
 package loadinglibrary.view
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.loading_view.view.*
+import loadinglibrary.LoadingManager
 import loadinglibrary.listener.DrawFinishListener
 import superlht.com.loadinglibrary.R
 import java.util.*
@@ -11,23 +14,48 @@ import java.util.*
 /**
  * Created by Hatim Liu on 2017/8/16.
  */
-class HTLoading(val context: Context) : DrawFinishListener {
+class HTLoading(context: Context) : DrawFinishListener {
 
 
-    val view: View = LayoutInflater.from(context).inflate(R.layout.loading_view, null)
-    val viewList = ArrayList<View>()
+    private val view: View = LayoutInflater.from(context).inflate(R.layout.loading_view, null)
+    private val viewList = ArrayList<View>()
+
+    private var interceptBack = true
+
+    private val loadingDialog = Dialog(context, R.style.loading_dialog)
+
+    private var successStr: String = manager.successText
 
     init {
         viewList.add(view.failedView)
         viewList.add(view.loadingView)
         viewList.add(view.successView)
         view.failedView.setOnDrawFinishListener(this)
+        loadingDialog.setCancelable(!interceptBack)
+        loadingDialog.setContentView(view.loading_dialog, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT))
+        initStyle()
+    }
+
+    private fun initStyle() {
+        if (manager != null) {
+            manager?.contentSize?.let { setSize(it) }
+            manager?.failedText?.let { }
+        }
+    }
+
+    fun Dialog.onBackPressed() {
+        if (interceptBack) {
+            return
+        }
+        dismiss()
     }
 
     fun show() {
+
     }
 
     fun dismiss() {
+        viewList.clear()
 
     }
 
@@ -47,6 +75,24 @@ class HTLoading(val context: Context) : DrawFinishListener {
 
     }
 
+    private fun setSuccessText(string: String) {
+
+
+    }
+
+    private fun hideAll() {
+        for (view in viewList) {
+            view.visibility = View.GONE
+        }
+    }
+
     override fun onDrawFinished(view: View) {
+    }
+
+    companion object {
+        private var manager: LoadingManager = LoadingManager()
+        fun init(manager: LoadingManager) {
+            this.manager = manager
+        }
     }
 }
--- sample/src/main/java/superlht/com/htloading/MainActivity.kt
+++ sample/src/main/java/superlht/com/htloading/MainActivity.kt
@@ -2,11 +2,14 @@ package superlht.com.htloading
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import loadinglibrary.LoadingManager
+import loadinglibrary.view.HTLoading
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        HTLoading.init(LoadingManager())
     }
 }
