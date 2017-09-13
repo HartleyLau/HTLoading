a0a4cccb13dd12b5d75723006f07d08dea6de271
HaiTian <476183603@qq.com>
2017-09-13 17:20:32 +0800

    完成sample

--- sample/src/main/java/superlht/com/htloading/MainActivity.kt
+++ sample/src/main/java/superlht/com/htloading/MainActivity.kt
@@ -2,19 +2,19 @@ package superlht.com.htloading
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import loadinglibrary.view.HTLoading
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val loading = HTLoading(this)
-        doAsync {
-            Thread.sleep(1000)
-            uiThread { loading.showFailed() }
+        listView.setOnItemClickListener { _, _, p, _ ->
+            when (p) {
+                0 -> startActivity<DefaultActivity>()
+                1 -> startActivity<CustomActivity>()
+            }
         }
     }
 }
